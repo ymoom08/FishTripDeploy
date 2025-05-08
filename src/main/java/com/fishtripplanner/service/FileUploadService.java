bbc210faec4e51bb8c3d92455d33ff390efe15ee
Your Name <you@example.com>
2025-05-08 16:52:53 +0900

    2025-5-8

--- src/main/java/com/fishtripplanner/service/FileUploadService.java
+++ src/main/java/com/fishtripplanner/service/FileUploadService.java
@@ -3,7 +3,6 @@ package com.fishtripplanner.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
